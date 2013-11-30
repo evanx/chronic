2b90f5f5e834338e66d1f3a494af60017a9f1315
Evan Summers <evan.summers@gmail.com>
2013-11-30 19:48:25 +0200

    öö

---
+++ test/chronic/scripts/mta.sh
@@ -0,0 +1,19 @@
+
+domain=test.org
+commonName=`hostname -s`.$domain
+org=$domain
+orgUnit=localnet
+
+if [ ! -f key.pem ] 
+then
+  rm -f cert.pem
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out cert.pem \
+    -subj "/CN=$commonName/OU=$orgUnit/O=$org"
+  openssl x509 -text -in cert.pem | grep CN
+  echo | openssl s_client -connect localhost:8443 2>/dev/null | 
+    sed -n -e '/BEGIN CERT/,/END CERT/p' > server.pem
+  openssl x509 -text -in server.pem | grep 'CN='
+fi
+
+tee ~/tmp/chronic.out | curl -k --key key.pem --data-binary @- -H 'Content-Type: text/plain' \
+  https://localhost:8443/post
