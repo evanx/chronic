c7ce7148262dc367ec9faa9ee2395f80138ef47e
Evan Summers <evan.summers@gmail.com>
2013-12-01 21:41:01 +0200

    öö

--- test/chronic/scripts/testclient.sh
+++ test/chronic/scripts/testclient.sh
@@ -3,16 +3,16 @@ cd ~/chronic/testclient
 
 cat cronmail.txt | grep -v OK | 
   curl --cacert server.pem --data-binary @- --key key.pem \
-  https://localhost:8443/
+  https://localhost:8443/post
 
 cat cronmail.txt | grep -v OK | 
   curl --cacert server.pem --data-binary @- --key key.pem \
-  https://localhost:8443/
+  https://localhost:8443/post
 
 cat cronmail.txt | grep -v CRIT | 
   curl --cacert server.pem --data-binary @- --key key.pem \
-  https://localhost:8443/
+  https://localhost:8443/post
 
 cat cronmail.txt | grep -v CRIT | 
   curl --cacert server.pem --data-binary @- --key key.pem \
-  https://localhost:8443/
+  https://localhost:8443/post
